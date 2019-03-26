35346d2f3458e30ae51af6a0c9138e88bf47e03d
DarlanJurak <darlan.jurak@gmail.com>
2019-03-26 14:55:13 -0300

    minor

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 #include <queue>            // frontier
 #include <iostream>         // "debug"
 #include <algorithm>        // std::reverse
+#include <unordered_set>
 
 struct Graph{
 
@@ -14,7 +15,63 @@ struct Graph{
 
 };
 
+struct Pos{
+
+    unsigned int x;
+    unsigned int y;
+
+};
+
+struct SquareGrid {
+  static std::array<Pos, 4> DIRS;
+
+  int width, height;
+  std::unordered_set<Pos> walls;
+
+  SquareGrid(int width_, int height_)
+     : width(width_), height(height_) {}
+
+  bool in_bounds(Pos id) const {
+    return 0 <= id.x && id.x < width
+        && 0 <= id.y && id.y < height;
+  }
+
+  bool passable(Pos id) const {
+    return walls.find(id) == walls.end();
+  }
+
+  std::vector<Pos> neighbors(Pos id) const {
+    std::vector<Pos> results;
+
+    for (Pos dir : DIRS) {
+      Pos next{id.x + dir.x, id.y + dir.y};
+      if (in_bounds(next) && passable(next)) {
+        results.push_back(next);
+      }
+    }
+
+    if ((id.x + id.y) % 2 == 0) {
+      // aesthetic improvement on square grids
+      std::reverse(results.begin(), results.end());
+    }
+
+    return results;
+  }
+};
+
+
 std::vector<char> BreadthFirstSearch(Graph, char start, char goal);
+
+namespace std {
+    /* implement hash function so we can put Pos into an unordered_set */
+    template <> struct hash<Pos> {
+        typedef Pos argument_type;
+        typedef std::size_t result_type;
+        std::size_t operator()(const Pos& id) const noexcept {
+            return std::hash<int>()(id.x ^ (id.y << 4));
+        }
+    };
+}
 
 int main(){
 
@@ -37,6 +94,13 @@ int main(){
     }};
 
     std::vector<char> path = BreadthFirstSearch(graph,'S', 'G');
+
+    // printing path
+    std::cout << "Path: ";
+    for(auto node : path){
+        std::cout << node;
+    }
+    std::cout << std::endl;
     
     return 0;
 }
@@ -69,7 +133,7 @@ std::vector<char> BreadthFirstSearch(Graph graph, char start, char goal){
         }
     }
 
-    for(auto element : came_from){                                                  // cout nodes and their parent
+    for(auto element : came_from){                                                  // cout nodes and theirs parent
         std::cout << element.first << " came from " << element.second << std::endl; 
     }
 
@@ -81,13 +145,6 @@ std::vector<char> BreadthFirstSearch(Graph graph, char start, char goal){
     }
     path.push_back(start);
     std::reverse(path.begin(), path.end());
-
-    // printing path
-    std::cout << "Path: ";
-    for(auto node : path){
-        std::cout << node;
-    }
-    std::cout << std::endl;
     
     return path;
 
